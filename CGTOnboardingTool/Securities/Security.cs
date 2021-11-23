1c24526457e959c3350fbb30b5dcc9727e313151
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2021-11-23 23:30:32 +0000

    Closes #30 A Security class with constructor and properties

---
+++ CGTOnboardingTool/Securities/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGTOnboardingTool.Securities
+{
+    public class Security
+    {
+        public String Name { get; set; }
+        public String ShortName { get; set; }
+        public double Section104 { get; set; }
+        public List<Security>? RelatedByRebuild { get; set; }
+        
+        public Security(string Name, String ShortName)
+        {
+            this.Name = Name;
+            this.ShortName = ShortName;
+            Section104 = 0;
+            RelatedByRebuild = new List<Security>();
+        }
+    }
+}

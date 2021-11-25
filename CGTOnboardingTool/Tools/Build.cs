8a9b7dc62f2c8d2efe79797269cbf09f8a7e4e38
Areeb Khan <2455329k@student.gla.ac.uk>
2021-11-25 14:23:00 +0000

    method stub for build

--- CGTOnboardingTool/Tools/Build.cs
+++ CGTOnboardingTool/Tools/Build.cs
@@ -8,6 +8,17 @@ namespace CGTOnboardingTool.Tools
 {
     public class Build
     {
+        //security saved as string till class avialabel 
+        string security = "apple";
+        int no_shares = 18;
+        int pps = 2;
+        int buycost = 10;
 
+        public float performBuild(string security, int no_shares, int pps, int buy_cost) {
+
+            float s104p = (no_shares * pps) + buy_cost;
+
+            return s104p;
+        }
     }
 }

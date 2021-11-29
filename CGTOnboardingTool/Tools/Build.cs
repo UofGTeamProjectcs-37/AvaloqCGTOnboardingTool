ff66ef2eac4c280414cad361d258ca6e1a928c40
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2021-11-29 17:56:11 +0000

    Updated Build to extend CGTFunction abstract class

--- CGTOnboardingTool/Tools/Build.cs
+++ CGTOnboardingTool/Tools/Build.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CGTOnboardingTool.Securities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,26 +7,51 @@ using System.Threading.Tasks;
 
 namespace CGTOnboardingTool.Tools
 {
-    public class Build
+    public class Build : CGTFunction
     {
-        
+
+        // Use these instead
+        Security security;
+        int quantity;
+        decimal pps;
+        decimal cost;
+        decimal gross;
+        DateOnly date;
+
+
+
+        // Old stub variables
         string security_name = "Apple";
         string short_name = "AAPL";
         int no_shares = 18;
-        double pps = 217;
-        double cost = 10;
-        
+        //double pps = 217;
+        //double cost = 10;
         double gross;
 
 
+        public Build(Security security, int quantity, decimal pps, decimal cost, DateOnly date)
+        {
+            this.security = security;
+            this.quantity = quantity;
+            this.pps = pps;
+            this.cost = cost;
+            this.date = date
+        }
 
-        public void performBuild(string name, string short_name, int no_shares, int pps, double cost) {
+        public override Report.ReportEntry perform(ref Report report)
+        {
+            // implement the Build Funciton here using appropriate Report methods (Such as UpdateSection104)
+            // return the report entry given when the Build is added to the report using report.Add()
 
-            Securities.Security new_security = new Securities.Security(name, short_name);  
+
+            return new Report.ReportEntry();
+        }
+
+        public void performBuild(string name, string short_name, int no_shares, int pps, double cost)
+        {
+            Securities.Security new_security = new Securities.Security(name, short_name);
 
             new_security.Section104 = (no_shares * pps) + cost;
-
-
         }
     }
 }

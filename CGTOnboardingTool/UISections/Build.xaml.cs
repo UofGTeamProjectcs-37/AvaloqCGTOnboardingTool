843e0c6170b0432dc4170ab97a15cbbaa3f6a0d5
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2021-11-29 18:45:39 +0000

    Fixed navigation for Build to Dashboard

--- CGTOnboardingTool/UISections/Build.xaml.cs
+++ CGTOnboardingTool/UISections/Build.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CGTOnboardingTool.UISections
 {
@@ -26,18 +14,11 @@ namespace CGTOnboardingTool.UISections
         {
             InitializeComponent();
             this.report = report;
-        }
+        
 
-        private void btnBuildOk_Click(object sender, RoutedEventArgs e)
+        private void BtnBuildCancel_Click(object sender, RoutedEventArgs e)
         {
-
-            this.NavigationService.GoBack();
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            this.NavigationService.Navigate(new Dashboard(ref report));
         }
     }
 }

be316ba981ec9e534b112c4e52fb795decd7446b
Joachim Vanneste <2460800v@student.gla.ac.uk>
2021-11-29 05:23:16 -0800

    Changes to performBuild function

--- CGTOnboardingTool/Tools/Build.cs
+++ CGTOnboardingTool/Tools/Build.cs
@@ -8,15 +8,18 @@ namespace CGTOnboardingTool.Tools
 {
     public class Build
     {
-        //security saved as string till class avialabel 
+        //security saved as string until securities class made
         string security = "apple";
         int no_shares = 18;
         int pps = 2;
-        int buycost = 10;
+        float cost = 10;
+        float gross;
 
-        public float performBuild(string security, int no_shares, int pps, int buy_cost) {
 
-            float s104p = (no_shares * pps) + buy_cost;
+
+        public float performBuild(string security, int no_shares, int pps, float cost, float gross) {
+
+            float s104p = (no_shares * pps) + cost;
 
             return s104p;
         }

b93f13aa233b02bfe551e7bc8eba0cacb9e812ab
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2021-12-01 00:05:02 +0000

    Change method calls to account for change in method signatures

--- CGTOnboardingTool/UISections/Dashboard.xaml.cs
+++ CGTOnboardingTool/UISections/Dashboard.xaml.cs
@@ -29,21 +29,34 @@ namespace CGTOnboardingTool.UISections
             this.Report = report;
             
             Security security = new Security("GlaxoSmithKlien","GSK");
-            int qty = 1000;
+            int qty = 500;
             decimal price = (decimal)16.35;
             decimal cost = (decimal)23.80;
             DateOnly date = new DateOnly(2021, 11, 30);
 
-            Tools.Build b = new Tools.Build(security,qty,price,cost,0,date);
+            Tools.Build b = new Tools.Build(security,qty,price,cost,date);
             b.perform(ref report);
 
-           
             int qty2 = 500;
             decimal price2 = (decimal)1.85;
             decimal cost2 = (decimal)5;
 
-            Tools.Build b2 = new Tools.Build(security, qty2,price2,cost2,0,date);
+            Tools.Build b2 = new Tools.Build(security, qty2,price2,cost2,date);
             b2.perform(ref report);
+
+            int qty3 = 500;
+            decimal price3 = (decimal)18.24;
+            decimal cost3 = (decimal)24.50;
+           
+            Tools.Reduce r = new Tools.Reduce(security,qty3,price3,cost3,date);
+            r.perform(ref report);
+
+            Security security2 = new Security("Tesla", "TSLA");
+            int rQ = 250;
+            int bQ = 50;
+
+            Tools.Rebuild rb = new Tools.Rebuild(security,rQ,security2,bQ,date);
+            rb.perform(ref report);
 
             this.DashboardReportView.ItemsSource = Report.Rows();
            

416ef163468379eedf3a61f34c0921572aab964d
Andrew Bell <2466420b@student.gla.ac.uk>
2022-02-08 21:11:04 +0000

    Moves Save and Open buttons from MainWindow to Dashboard and adds basic implementation for custom ComboBox look

--- CGTOnboardingTool/MainWindow.xaml.cs
+++ CGTOnboardingTool/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -49,59 +47,5 @@ namespace CGTOnboardingTool
             mainFrame.Navigate(new UISections.Rebuild(ref report));
 
         }
-
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-
-            List<ReportEntry> t = Report.Rows();
-           
-
-            Stream myStream;
-            SaveFileDialog saveFile = new SaveFileDialog();
-            saveFile.Filter = "txt files (*.txt) | *.*" ;
-            saveFile.FilterIndex = 2;
-            saveFile.RestoreDirectory = true;
-            UnicodeEncoding uniEncoding = new UnicodeEncoding();
-            if (saveFile.ShowDialog() == true)
-            {
-                if ((myStream = saveFile.OpenFile())!=null) {
-
-                    for (int  i=0; i<Report.Count(); i++)
-                    {
-                        char[] row = { Convert.ToChar(t[i].EntryID), Convert.ToChar(t[i].FunctionPerformed) };
-                    
-                        myStream.Write(uniEncoding.GetBytes(row));
-                    }
-                    myStream.Close();
-                }
-            }
-          
-           
-            
-            for (int i=0; i<Report.Count();i++)
-            {
-                System.Diagnostics.Debug.WriteLine(t[i].EntryID);
-                System.Diagnostics.Debug.WriteLine(t[i].DatePerformed);
-                System.Diagnostics.Debug.WriteLine(t[i].FunctionPerformed);
-                System.Diagnostics.Debug.WriteLine(t[i].SecuritiesAffected);
-                System.Diagnostics.Debug.WriteLine(t[i].PricesAffected);
-                System.Diagnostics.Debug.WriteLine(t[i].QuantitiesAffected);
-                System.Diagnostics.Debug.WriteLine(t[i].AssociatedCosts);
-                System.Diagnostics.Debug.WriteLine(t[i].Section104sAfter);
-            }
-
-         
-
-
-
-
-        }
-
-        private void btnOpen_Click(object sender, RoutedEventArgs e)
-        {
-            
-
-        }
-
     }
 }
--- CGTOnboardingTool/UISections/Dashboard.xaml.cs
+++ CGTOnboardingTool/UISections/Dashboard.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CGTOnboardingTool.Securities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace CGTOnboardingTool.UISections
 {
@@ -152,6 +154,47 @@ namespace CGTOnboardingTool.UISections
                 LblReportFilterDateFrom.Visibility = Visibility.Visible;
                 LblReportFilterDateTo.Visibility = Visibility.Visible;
             }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+           /* List<ReportEntry> t = Report.Rows();
+
+
+             Stream myStream;
+             SaveFileDialog saveFile = new SaveFileDialog();
+             saveFile.Filter = "txt files (*.txt) | *.*" ;
+             saveFile.FilterIndex = 2;
+             saveFile.RestoreDirectory = true;
+             UnicodeEncoding uniEncoding = new UnicodeEncoding();
+             if (saveFile.ShowDialog() == true)
+             {
+                 if ((myStream = saveFile.OpenFile())!=null) {
+
+                     for (int  i=0; i<Report.Count(); i++)
+                     {
+                         char[] row = { Convert.ToChar(t[i].EntryID), Convert.ToChar(t[i].FunctionPerformed) };
+
+                         myStream.Write(uniEncoding.GetBytes(row));
+                     }
+                     myStream.Close();
+                 }
+             }
+
+
+
+             for (int i=0; i<Report.Count();i++)
+             {
+                 System.Diagnostics.Debug.WriteLine(t[i].EntryID);
+                 System.Diagnostics.Debug.WriteLine(t[i].DatePerformed);
+                 System.Diagnostics.Debug.WriteLine(t[i].FunctionPerformed);
+                 System.Diagnostics.Debug.WriteLine(t[i].SecuritiesAffected);
+                 System.Diagnostics.Debug.WriteLine(t[i].PricesAffected);
+                 System.Diagnostics.Debug.WriteLine(t[i].QuantitiesAffected);
+                 System.Diagnostics.Debug.WriteLine(t[i].AssociatedCosts);
+                 System.Diagnostics.Debug.WriteLine(t[i].Section104sAfter);
+             }*/
         }
     }
     public class Entry

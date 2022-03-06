c6a212e0af504d58b2715992a93b6ac10e2ab56e
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-03-06 13:50:58 +0000

    BUC classes documentation added

--- CGTOnboardingTool/Tools/CGTFunction.cs
+++ CGTOnboardingTool/Tools/CGTFunction.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace CGTOnboardingTool.Tools
 {
+    // Abstract function for BUC's to subclass 
     public abstract class CGTFunction
     {
         public abstract ReportEntry perform(ref Report report);
 
         public override string ToString()
         {
+            // Return name of BUC 
             string[] function = base.ToString().Split('.');
             return function[2];
         }

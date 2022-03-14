eb718b15d65200de75bbf70c90923836cf7d2d03
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-03-14 16:58:44 +0000

    import stream added

--- CGTOnboardingTool/Models/OutputModels/ReportImporter.cs
+++ CGTOnboardingTool/Models/OutputModels/ReportImporter.cs
@@ -1,4 +1,6 @@
 ﻿using CGTOnboardingTool.Models.DataModels;
+using System.IO;
+
 
 namespace CGTOnboardingTool.Models.OutputModels
 {
@@ -9,6 +11,34 @@ namespace CGTOnboardingTool.Models.OutputModels
         public ReportImporter(ref Report report)
         {
             this.report = report;
+        }
+
+        // Import a report from txt or csv file
+        public void ImportReport() 
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                        // Insert code to read the stream here.
+                        }
+                    }
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Error: " + ex.Message);
+                }       
+            }
+
+
         }
 
     }
